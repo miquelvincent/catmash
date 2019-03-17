1a2517ff2f64a7950b2cf68685ad359010bc2b46
Vincent Miquel <miquelvincent@me.com>
2019-03-17 10:21:31 +0100

    [API] Set a new /get ranking route

--- api/controllers/cat.controller.js
+++ api/controllers/cat.controller.js
@@ -1,6 +1,6 @@
 const Ranking = require('../models/cat.model');
 
-exports.cat_add = function(req, res, next) {
+exports.cat_add = (req, res, next) => {
   let query = {id: req.body.id};
   Ranking.findOneAndUpdate(
     query,
@@ -11,4 +11,10 @@ exports.cat_add = function(req, res, next) {
       return res.send('Succesfully saved  🎊');
     },
   );
+};
+
+exports.send_cat = (req, res, next) => {
+  Ranking.find({}, (err, cats) => {
+    res.send(cats);  
+  });
 };
--- api/routes/cat.route.js
+++ api/routes/cat.route.js
@@ -5,5 +5,6 @@ const cat_controller = require('../controllers/cat.controller');
 
 
 router.post('/ranking', cat_controller.cat_add);
+router.get('/ranking', cat_controller.send_cat);
 
 module.exports = router;
